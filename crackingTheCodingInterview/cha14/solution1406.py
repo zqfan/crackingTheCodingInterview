ecbd30ca8fe2ee2d7e5cbd2f8fd2e957ac7625ad
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-07 01:41:12 +0800

    solution 1406 done

---
+++ crackingTheCodingInterview/cha14/solution1406.py
@@ -0,0 +1,11 @@
+"""
+Suppose you are using a map in your program, how would you count the
+number of times the program calls the put() and get() functions?
+
+analyze:
+in python:
+
+1. subclass map class as MyMap
+2. add a class attr count to MyMap
+3. when doing put() and get(), increase count by 1
+"""
