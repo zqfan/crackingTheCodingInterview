fa01994ef16d1111e0e4b07122013b56ce586c34
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-06 20:36:25 +0800

    solution 1203 done

---
+++ crackingTheCodingInterview/cha12/solution1203.py
@@ -0,0 +1,11 @@
+"""
+We have the following method used in a chess game: boolean canMoveTo(int x, int y) x and y are the coordinates of the chess board and it returns whether or not the piece can move to that position.
+Explain how you would test this method.
+
+Analyze:
+* input negative integer of x, y
+* input larger value of x, y, which bigger than bound value
+* input bound value of x, y, such as 0 or/and max value
+* known value, some value which will produce know result
+* some random valid value, just check if it will crash
+"""
