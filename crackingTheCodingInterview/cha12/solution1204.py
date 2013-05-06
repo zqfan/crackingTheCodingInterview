0c7bcdd42279fed0b5de9e5647a018cd7901fd04
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-06 20:43:25 +0800

    solution 1204 done

---
+++ crackingTheCodingInterview/cha12/solution1204.py
@@ -0,0 +1,10 @@
+"""
+How would you load test a webpage without using any test tools?
+
+analyze:
+* test average load time
+* test max connection
+* test out bandwidth
+* test sql injection
+* test web browser compatibility
+"""
