57da3acc83ac1fdc3685e9d4c64c14528fc9b00d
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-06 20:53:22 +0800

    solution 1005 done

---
+++ crackingTheCodingInterview/cha12/solution1205.py
@@ -0,0 +1,21 @@
+"""
+How would you test a pen?
+
+analyze:
+
+* what the major user group of this pen?
+* if we need to satisfy other people?
+* what this pen used to do? i.e. write on paper or board
+* what ink this pen is using?
+
+assuming that the pen is used by worker who will often use this pen to
+draw thin line on paper, so we test
+
+* different ink, worker may use different ink, we should test common
+ink should work with the pen
+* average length of 10ml ink, if we just draw lines, how long these
+lines will be
+* temperature test, if the pen will work stably from 0'C - 50'C?
+* pressure test, when air pressure is low or high, if it will work?
+* pressure test, if we press the pen, how heavy it can bear
+"""
