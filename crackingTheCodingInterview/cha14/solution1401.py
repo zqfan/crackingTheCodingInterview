95ab4cf76cfedd41011b3f06f8cfbdd8900fdbd2
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-06 23:45:07 +0800

    solution 1401 done

---
+++ crackingTheCodingInterview/cha14/solution1401.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+"""
+In terms of inheritance, what is the effect of keeping a constructor
+private?
+
+Analyze:
+in java, if constructor is private
+
+* you can not instantiate that class
+* you can not inherit from that class
+
+in python, constructor is __init__ method, it is special method, and
+cannot be treated as private constructor
+"""
