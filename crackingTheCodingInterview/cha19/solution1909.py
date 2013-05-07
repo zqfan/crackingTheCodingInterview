fc04af6a635123bfcd96c6b4c9dd1bb28182d0e1
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-07 17:34:33 +0800

    give up solution 1909

---
+++ crackingTheCodingInterview/cha19/solution1909.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+"""
+Since XML is very verbose, you are given a way of encoding it where
+each tag gets mapped to a pre-defined integer value. The
+language/grammar is as follows:
+Element --> Element Attr* END Element END [aka, encode the element
+tag, then its attributes, then tack on an END character, then encode
+its children, then another end tag]
+Attr --> Tag Value [assume all values are strings]
+END --> 01
+Tag --> some predefined mapping to int
+Value --> string value END
+Write code to print the encoded version of an xml element (passed in
+as string).
+FOLLOW UP
+Is there anything else you could do to (in many cases) compress this
+even further?
+"""
+
+def encode_xml(strxml):
+    """
+
+    i think the grammar is not fully defined
+    """
+    pass
