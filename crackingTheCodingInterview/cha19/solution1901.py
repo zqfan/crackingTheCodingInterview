5e97744646612ceb59b5560ea012bb33cd095476
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-07 11:47:27 +0800

    solution 1901 done

---
+++ crackingTheCodingInterview/cha19/solution1901.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+"""
+Write a function to swap a number in place without temporary
+variables.
+"""
+
+def swap(a, b):
+    a, b = b, a
