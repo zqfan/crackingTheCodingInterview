89924c93f52cfed6a3a8873725fd373a7192ccbd
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-10 12:13:05 +0800

    give up solution 2013 because brute force

---
+++ crackingTheCodingInterview/cha20/solution2013.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+"""
+Given a dictionary of millions of words, give an algorithm to find the
+largest possible rectangle of letters such that every row forms a word
+(reading left to right) and every column forms a word (reading top to
+bottom).
+"""
+
+def get_max_puzzle(words):
+    """
+    I only can get a brute force algorithm, which seems boring, so i
+    give up
+    """
+    pass
