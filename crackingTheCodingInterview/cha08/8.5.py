e2beda90936c4147e9c4b74cab52fb761951c420
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-29 09:36:48 +0800

    correct 8.5 solution

--- crackingTheCodingInterview/cha08/8.5.py
+++ crackingTheCodingInterview/cha08/8.5.py
@@ -15,19 +15,28 @@ actually, there is another combination for n=3
 (()())
 """
 
+
 def parentheses(n):
     """Return a set of all valid n-pairs of parentheses."""
     if n < 1:
         return set([])
-    if n == 1:
-        return set(["()"])
-    result = set([])
-    r = parentheses(n-1)
-    for rr in r:
-        result.add("()"+rr)
-        result.add(rr+"()")
-        result.add("("+rr+")")
-    return result
+    result_set = set([])
+
+    def get_paren_pair(left, right, parens):
+        if (left < 0 or right < left):
+            return
+        if (left == 0 and right == 0):
+            result_set.add(parens)
+            return
+        if (left > 0):
+            new_parens = parens + '('
+            get_paren_pair(left - 1, right, new_parens)
+        if (right > left):
+            new_parens = parens + ')'
+            get_paren_pair(left, right - 1, new_parens)
+
+    get_paren_pair(n, n, '')
+    return result_set
 
 
 if __name__ == "__main__":
